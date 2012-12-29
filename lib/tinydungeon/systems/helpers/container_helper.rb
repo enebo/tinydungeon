1b3ebf3b710728be1f1ec36d7b5878e132e7edf6
Thomas E. Enebo <tom.enebo@gmail.com>
2012-12-29 13:39:11 -0600

    Some more transactions

--- lib/tinydungeon/systems/helpers/container_helper.rb
+++ lib/tinydungeon/systems/helpers/container_helper.rb
@@ -4,16 +4,20 @@ module ContainerHelper
   ##
   # Add an entity to a container entity
   def add_to_container(container, entity)
-    must_be_container(container)
-    entity.has ContainedBy.new(container)
-    container.add Containee.new(entity)
+    manager.transaction do
+      must_be_container(container)
+      entity.has ContainedBy.new(container)
+      container.add Containee.new(entity)
+    end
   end
 
   def swap_containers(old_container, new_container, entity)
-    must_be_container(new_container)
-    old_container.many(Containee).each { |l| l.delete if l.same? entity.id }
-    entity.one(ContainedBy).value = new_container.id
-    new_container.add Containee.new(entity)
+    manager.transaction do
+      must_be_container(new_container)
+      old_container.many(Containee).each { |l| l.delete if l.same? entity.id }
+      entity.one(ContainedBy).value = new_container.id
+      new_container.add Containee.new(entity)
+    end
   end
 
   def container?(entity)
